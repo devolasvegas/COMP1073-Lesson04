438be4c6b5a91076960840599d144cf43574c199
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-02-04 10:39:33 -0500

    Initial Commit

---
+++ Scripts/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by devon on 2016-02-04.
+ */
+
+"use strict";
+
+// Set up your IIFE (Immediately Invoked Function Expression)
+(function(){
+    console.log("App Started . . .");
+})();
