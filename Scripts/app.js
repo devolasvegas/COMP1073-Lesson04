2d10a76cc4375a324d8fa885ac7582524619775a
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-02-04 11:46:58 -0500

    Updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -4,7 +4,18 @@
 
 "use strict";
 
-// Set up your IIFE (Immediately Invoked Function Expression)
+// Set up your IIFE (Immediately Invoked Function Expression) or ASEF (Anonymous Self-Executing Function)
 (function(){
     console.log("App Started . . .");
+
+    // named function way
+    function replaceFirstParagraph(){
+        console.log("Inside replaceFirstParagraph function.");
+        var firstParagraph;
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "My New Paragraph Data";
+    }
+    // Call replaceFirstParagraph function defined above.
+    replaceFirstParagraph();
+
 })();
