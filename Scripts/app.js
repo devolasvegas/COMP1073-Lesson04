5d208815fc159630c4c619917e934aa6c98e5bf4
DEVON DAVIAU <devon.daviau@yahoo.ca>
2016-02-04 11:52:32 -0500

    Add anonymous function to app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -8,6 +8,7 @@
 (function(){
     console.log("App Started . . .");
 
+    /*
     // named function way
     function replaceFirstParagraph(){
         console.log("Inside replaceFirstParagraph function.");
@@ -15,6 +16,18 @@
         firstParagraph = document.getElementById("firstParagraph");
         firstParagraph.innerHTML = "My New Paragraph Data";
     }
+     */
+
+
+
+    //declare an anon. function with named alias
+    var replaceFirstParagraph = function(){
+        console.log("Inside replaceFirstParagraph anonymous function.");
+        var firstParagraph;
+        firstParagraph = document.getElementById("firstParagraph");
+        firstParagraph.innerHTML = "Newer content.";
+    }
+
     // Call replaceFirstParagraph function defined above.
     replaceFirstParagraph();
 
